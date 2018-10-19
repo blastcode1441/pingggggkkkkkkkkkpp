ea83c202caf1bc863fbbb97410d97503ea39dd01
juliandaw <33635418+juliandaw@users.noreply.github.com>
2018-10-19 14:34:42 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,14 +4,6 @@ const client = new Discord.Client();
 
 const prefix = "-"
 
-
-
-console.log('=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=')
-
-console.log(' [Wait please .. ] ')
-
-console.log('=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=')
-
 client.on('ready', () => {
 
 	console.log('')
@@ -58,10 +50,7 @@ client.on('ready', () => {
 
 });
 
- const devs = ['389090790984515594','337629134371160065'];
-
-const adminprefix = "-"
-
+  
 
 
 
@@ -76,163 +65,10 @@ client.on("ready", () => {
 
 const code = '-';
 
-client.on('message',async message => {
 
-    if(message.content.startsWith(code + "js")) {
 
-  if(!message.channel.guild) return message.reply(' ');
-
-    let rank = message.guild.member(message.author).roles.find('name', '⇒ Support');
-
-    if (!rank) return message.channel.send(':octagonal_sign: **| يجب ان تمتلك رتبة سبورت لأستخدام هذا الأمر.**');
-
-    let jscodes = message.guild.channels.find('name', "code-js");
-
-    if(!jscodes) return message.channel.send(":x:لم اجد الروم الخاص بنشر الاكواد");
-
-      let filter = m => m.author.id === message.author.id;
-
-      let thisMessage;
-
-      let thisFalse;
-
-      message.channel.send(':pencil: **| من فضلك اكتب الكود الأن... :pencil2: **').then(msg => {
-
-  
-
-      message.channel.awaitMessages(filter, {
-
-        max: 1,
-
-        time: 90000,
-
-        errors: ['time']
-
-      })
-
-      .then(collected => {
-
-        collected.first().delete();
-
-        thisMessage = collected.first().content;
-
-        let boi;
-
-        msg.edit(':scroll: **| من فضلك اكتب وصف الكود الأن... :pencil2: **').then(msg => {
-
-  
-
-            message.channel.awaitMessages(filter, {
-
-              max: 1,
-
-              time: 90000,
-
-              errors: ['time']
-
-            })
-
-            .then(collected => {
-
-              collected.first().delete();
-
-              boi = collected.first().content;
-
-              let boi2;
-
-              msg.edit(':man_in_tuxedo: **| من فضلك اكتب من صنع هذا الكود الأن... :pencil2: **').then(msg => {
-
-  
-
-                message.channel.awaitMessages(filter, {
-
-                  max: 1,
-
-                  time: 90000,
-
-                  errors: ['time']
-
-                })
-
-                .then(collected => {
-
-                  collected.first().delete();
-
-                boi2 = collected.first().content;
-
-        msg.edit(':shield: **| [ هل انت متأكد من نشر الكود؟ | [ نعم ] او [ لا**');
-
-   message.channel.awaitMessages(response => response.content === 'نعم' || 'لا' && filter,{
-
-          max: 1,
-
-          time: 90000,
-
-          errors: ['time']
-
-        })
-
-        .then(collected => {
-
-          if(collected.first().content === 'لا') {
-
-            msg.delete();
-
-            message.delete();
-
-            thisFalse = false;
-
-          }
-
-          if(collected.first().content === 'نعم') {
-
-            if(thisFalse === false) return;
-
-            msg.edit(':dove: **| Done :white_check_mark:, تم بنجاح نشر كودك في روم الاكواد**');
-
-            collected.first().delete();
-
-            jscodes.send(`@everyone | @here
-
-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-
-**Ping Development© :arrow_down:** 
-
-\`\`\`js
-
-${thisMessage}\`\`\`
-
-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-
-**وصف الكود**: ${boi}
-
-**تم النشر بواسطة**: ${message.author}
-
-**المصدر / الشخص الذي صنع الكود**: ${boi2}`); 
-
-          }
-
-        }
-
-    );
-
-});
-
-      });
-
-    }
-
-      );
-
-    });
-
-}
-
-);
-
-      })}});
-
-
+            
+              
       
 client.on('message',async message => {
 
