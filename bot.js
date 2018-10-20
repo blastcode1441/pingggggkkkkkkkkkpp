9737c2b47cf35de832cc75d807ae1f672221164b
juliandaw <33635418+juliandaw@users.noreply.github.com>
2018-10-20 23:38:04 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -95,20 +95,6 @@ client.on('message',async message => {
 
  
 
-client.on('message', message => {
-  if(message.content.startsWith("#credit <@470366007748198401>","#credits <@470366007748198401>")) {
-    let role = message.guild.roles.find("name", "Donator");
-    if(!role) {
-      return message.channel.send('Thanks for your support. ❤');
-    }
-      message.member.addRole(role);
-      let embed = new Discord.RichEmbed()
-        .setColor("RANDOM")
-        .setAuthor("Thanks for you support you have given the Donator role.");
-
-        message.author.sendEmbed(embed);
-  }
-});
 
 
 client.on('voiceStateUpdate', (u, member) => {
